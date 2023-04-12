77effca50430146640e0cf8e286e21a179c64f44
Bernardo Gelain Dariva <bernardogdariva@gmail.com>
2023-04-12 13:00:01 -0300

    assets

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes as RRDRouter, Route } from "react-router-dom";
+import Principal from "../components/principalPage/principal";
 import Second from "../components/secondParallax/second";
 
 import Template from "../components/template/template";
@@ -7,7 +8,8 @@ export default function Routes() {
   return (
     <BrowserRouter>
       <RRDRouter>
-        <Route path="/" element={<Template />} />
+        <Route path="/" element={<Principal />} />
+        <Route path="/template" element={<Template />} />
         <Route path="/second" element={<Second />} />
       </RRDRouter>
     </BrowserRouter>
